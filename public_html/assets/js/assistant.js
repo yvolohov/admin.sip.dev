7c1fcefc3a42bfdb303eee92d1c7a44aad1ffc6d
Yaroslav Volohov <yvolohov@gmail.com>
2017-03-09 11:59:11 +0200

    generator builder

--- public_html/assets/js/assistant.js
+++ public_html/assets/js/assistant.js
@@ -1,3 +1,41 @@
-$(document).ready(function () {
-    console.log('assistant');
+$(document).ready(function ()
+{
+    function TemplatesBuilder(rawData)
+    {
+        this.rawData = rawData;
+
+        this.build = function ()
+        {
+            var parts = this.rawData.split('---\n');
+
+            for (var partId in parts)
+            {
+                var part = parts[partId];
+                var firstSymbol = part.substr(0, 1);
+
+                if (firstSymbol == '\n') {
+                    part = part.substr(1, part.length - 1);
+                }
+
+                var lastSymbol = part.substr(part.length - 1, 1);
+
+                if (lastSymbol == '\n') {
+                    part = part.substr(0, part.length - 1);
+                }
+
+                parts[partId] = part;
+            }
+
+            console.log(parts);
+            return this.rawData;
+        };
+    }
+
+    $("#button-generate").click(function ()
+    {
+        var rawData = $("#textarea-raw-data").val();
+        var templatesBuilder = new TemplatesBuilder(rawData);
+        var templates = templatesBuilder.build();
+        $("#textarea-templates").val(templates);
+    });
 });
